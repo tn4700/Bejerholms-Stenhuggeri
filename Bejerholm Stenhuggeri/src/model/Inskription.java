25dd927daac9aa78a40338122cbaaa46255dbfef
Martin Hana <double_pun@hotmail.com>
2013-05-04 01:38:30 +0200

    Inskription done

--- Bejerholm Stenhuggeri/src/model/Inskription.java
+++ Bejerholm Stenhuggeri/src/model/Inskription.java
@@ -12,5 +12,26 @@ public class Inskription {
     
     private String inskription;
     private Tegntype id;
+
+    public Inskription() {
+        this.inskription = inskription;
+        this.id = id;
+    }
+
+    public String getInskription() {
+        return inskription;
+    }
+
+    public void setInskription(String inskription) {
+        this.inskription = inskription;
+    }
+
+    public Tegntype getId() {
+        return id;
+    }
+
+    public void setId(Tegntype id) {
+        this.id = id;
+    }
        
 }
