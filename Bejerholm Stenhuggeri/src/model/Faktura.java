0e7d2e918d01e5b81b9afaf1a6eea9ed8342ba7a
Martin Hana <double_pun@hotmail.com>
2013-05-04 01:36:47 +0200

    Faktura done v2

--- Bejerholm Stenhuggeri/src/model/Faktura.java
+++ Bejerholm Stenhuggeri/src/model/Faktura.java
@@ -34,5 +34,85 @@ public class Faktura {
       this.ordre_nr = ordre_nr;
       this.bedemand_cvr = bedemand_cvr;
   }
+
+    public int getFaktura_nr() {
+        return faktura_nr;
+    }
+
+    public void setFaktura_nr(int faktura_nr) {
+        this.faktura_nr = faktura_nr;
+    }
+
+    public Timestamp getFaktureringsdato() {
+        return faktureringsdato;
+    }
+
+    public void setFaktureringsdato(Timestamp faktureringsdato) {
+        this.faktureringsdato = faktureringsdato;
+    }
+
+    public String getVedrørende() {
+        return vedrørende;
+    }
+
+    public void setVedrørende(String vedrørende) {
+        this.vedrørende = vedrørende;
+    }
+
+    public String getBetalingsbetingelser() {
+        return betalingsbetingelser;
+    }
+
+    public void setBetalingsbetingelser(String betalingsbetingelser) {
+        this.betalingsbetingelser = betalingsbetingelser;
+    }
+
+    public Timestamp getSendt_dato() {
+        return sendt_dato;
+    }
+
+    public void setSendt_dato(Timestamp sendt_dato) {
+        this.sendt_dato = sendt_dato;
+    }
+
+    public String getFaktureringsadresse() {
+        return faktureringsadresse;
+    }
+
+    public void setFaktureringsadresse(String faktureringsadresse) {
+        this.faktureringsadresse = faktureringsadresse;
+    }
+
+    public boolean isFakturatype() {
+        return fakturatype;
+    }
+
+    public void setFakturatype(boolean fakturatype) {
+        this.fakturatype = fakturatype;
+    }
+
+    public boolean isBetalingsstatus() {
+        return betalingsstatus;
+    }
+
+    public void setBetalingsstatus(boolean betalingsstatus) {
+        this.betalingsstatus = betalingsstatus;
+    }
+
+    public int getOrdre_nr() {
+        return ordre_nr;
+    }
+
+    public void setOrdre_nr(int ordre_nr) {
+        this.ordre_nr = ordre_nr;
+    }
+
+    public int getBedemand_cvr() {
+        return bedemand_cvr;
+    }
+
+    public void setBedemand_cvr(int bedemand_cvr) {
+        this.bedemand_cvr = bedemand_cvr;
+    }
     
 }
