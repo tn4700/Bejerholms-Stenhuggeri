c16b6e4510fb3e05611d3dc8607b25777d2ff941
Martin Hana <double_pun@hotmail.com>
2013-05-04 01:34:57 +0200

    Faktura done

--- Bejerholm Stenhuggeri/src/model/Faktura.java
+++ Bejerholm Stenhuggeri/src/model/Faktura.java
@@ -22,6 +22,17 @@ public class Faktura {
    private int ordre_nr;
    private int bedemand_cvr;
    
-    
+  private Faktura(){
+      this.faktura_nr = faktura_nr;
+      this.faktureringsdato = faktureringsdato;
+      this.vedrørende = vedrørende;
+      this.betalingsbetingelser = betalingsbetingelser;
+      this.sendt_dato = sendt_dato;
+      this. faktureringsadresse = faktureringsadresse;
+      this.fakturatype = fakturatype;
+      this.betalingsstatus = betalingsstatus;
+      this.ordre_nr = ordre_nr;
+      this.bedemand_cvr = bedemand_cvr;
+  }
     
 }
