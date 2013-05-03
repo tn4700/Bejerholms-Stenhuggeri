257243100e8a2ed72f44d65e7b1b701c448cc863
Martin Hana <double_pun@hotmail.com>
2013-05-04 01:51:57 +0200

    Kunde done

--- Bejerholm Stenhuggeri/src/model/Kunde.java
+++ Bejerholm Stenhuggeri/src/model/Kunde.java
@@ -15,5 +15,51 @@ public class Kunde {
     private int tlf;
     private Postnumre post_nr;
     
-    
+    public Kunde(){
+        this.fornavn=fornavn;
+        this.efternavn=efternavn;
+        this.adresse=adresse;
+        this.tlf=tlf;
+        this.post_nr=post_nr;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEfternavn() {
+        return efternavn;
+    }
+
+    public void setEfternavn(String efternavn) {
+        this.efternavn = efternavn;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public int getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(int tlf) {
+        this.tlf = tlf;
+    }
+
+    public Postnumre getPost_nr() {
+        return post_nr;
+    }
+
+    public void setPost_nr(Postnumre post_nr) {
+        this.post_nr = post_nr;
+    }
 }
