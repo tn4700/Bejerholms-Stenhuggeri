82bb6849703c0e756e38fc013cbe6c9b5f69363a
Martin Hana <double_pun@hotmail.com>
2013-05-04 01:46:10 +0200

    Kontoudtog_linje done

--- Bejerholm Stenhuggeri/src/model/Kontoudtog_linje.java
+++ Bejerholm Stenhuggeri/src/model/Kontoudtog_linje.java
@@ -16,4 +16,52 @@ public class Kontoudtog_linje {
     private float pris;
     private int kontoudtog_nr;
     
+    public Kontoudtog_linje(){
+        this.antal = antal;
+        this.linje_nr =linje_nr;
+        this.beskrivelse=beskrivelse;
+        this.pris=pris;
+        this.kontoudtog_nr=kontoudtog_nr;
+        
+    }
+
+    public int getAntal() {
+        return antal;
+    }
+
+    public void setAntal(int antal) {
+        this.antal = antal;
+    }
+
+    public int getLinje_nr() {
+        return linje_nr;
+    }
+
+    public void setLinje_nr(int linje_nr) {
+        this.linje_nr = linje_nr;
+    }
+
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
+
+    public void setBeskrivelse(String beskrivelse) {
+        this.beskrivelse = beskrivelse;
+    }
+
+    public float getPris() {
+        return pris;
+    }
+
+    public void setPris(float pris) {
+        this.pris = pris;
+    }
+
+    public int getKontoudtog_nr() {
+        return kontoudtog_nr;
+    }
+
+    public void setKontoudtog_nr(int kontoudtog_nr) {
+        this.kontoudtog_nr = kontoudtog_nr;
+    }
 }
