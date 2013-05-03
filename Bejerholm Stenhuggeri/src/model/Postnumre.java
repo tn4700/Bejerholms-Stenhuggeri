c9e95859318922b302655065ae0f910631592d02
Martin Hana <double_pun@hotmail.com>
2013-05-04 01:59:21 +0200

    Ordre done

--- Bejerholm Stenhuggeri/src/model/Postnumre.java
+++ Bejerholm Stenhuggeri/src/model/Postnumre.java
@@ -12,4 +12,24 @@ public class Postnumre {
     private int post_nr;
     private String byNavn;
     
+    public Postnumre(){
+        this.post_nr=post_nr;
+        this.byNavn=byNavn;
+    }
+
+    public int getPost_nr() {
+        return post_nr;
+    }
+
+    public void setPost_nr(int post_nr) {
+        this.post_nr = post_nr;
+    }
+
+    public String getByNavn() {
+        return byNavn;
+    }
+
+    public void setByNavn(String byNavn) {
+        this.byNavn = byNavn;
+    }
 }
