12aba71ec66f9c1fbb19167e03083aed35c369d6
Martin Hana <double_pun@hotmail.com>
2013-05-04 02:12:39 +0200

    Varegrupper done

--- Bejerholm Stenhuggeri/src/model/Varegrupper.java
+++ Bejerholm Stenhuggeri/src/model/Varegrupper.java
@@ -9,7 +9,29 @@ package model;
  * @author Martin
  */
 public class Varegrupper {
+
     private int grp_nr;
     private String navn;
-    
+
+    public Varegrupper() {
+        this.grp_nr = grp_nr;
+        this.navn = navn;
+
+    }
+
+    public int getGrp_nr() {
+        return grp_nr;
+    }
+
+    public void setGrp_nr(int grp_nr) {
+        this.grp_nr = grp_nr;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
 }
