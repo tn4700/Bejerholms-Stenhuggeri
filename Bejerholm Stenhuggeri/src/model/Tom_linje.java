598510e89aed5630bc4932c8241f6a54d6436da5
Martin Hana <double_pun@hotmail.com>
2013-05-04 02:09:48 +0200

    Tom_linje done

--- Bejerholm Stenhuggeri/src/model/Tom_linje.java
+++ Bejerholm Stenhuggeri/src/model/Tom_linje.java
@@ -9,11 +9,59 @@ package model;
  * @author Martin
  */
 public class Tom_linje {
-    
+
     private String navn;
     private float pris;
     private int antal;
     private String kommentar;
     private int id;
-    
+
+    public Tom_linje() {
+        this.navn = navn;
+        this.pris = pris;
+        this.antal = antal;
+        this.kommentar = kommentar;
+        this.id = id;
+
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public float getPris() {
+        return pris;
+    }
+
+    public void setPris(float pris) {
+        this.pris = pris;
+    }
+
+    public int getAntal() {
+        return antal;
+    }
+
+    public void setAntal(int antal) {
+        this.antal = antal;
+    }
+
+    public String getKommentar() {
+        return kommentar;
+    }
+
+    public void setKommentar(String kommentar) {
+        this.kommentar = kommentar;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
