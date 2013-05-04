1219c28c679cb1b1989668b6db2934716d95ae4f
Martin Hana <double_pun@hotmail.com>
2013-05-04 02:07:01 +0200

    Samarbejdspartnere done

--- Bejerholm Stenhuggeri/src/model/Samarbejdspartnere.java
+++ Bejerholm Stenhuggeri/src/model/Samarbejdspartnere.java
@@ -9,7 +9,7 @@ package model;
  * @author Martin
  */
 public class Samarbejdspartnere {
-    
+
     private String firmanavn;
     private String adresse;
     private int tlf;
@@ -18,6 +18,80 @@ public class Samarbejdspartnere {
     private int konto_nr;
     private String bank;
     private Postnumre post_nr;
-    
-    
+
+    public Samarbejdspartnere() {
+        this.firmanavn = firmanavn;
+        this.adresse = adresse;
+        this.tlf = tlf;
+        this.cvr_nr = cvr_nr;
+        this.registrerings_nr = registrerings_nr;
+        this.konto_nr = konto_nr;
+        this.bank = bank;
+        this.post_nr = post_nr;
+
+    }
+
+    public String getFirmanavn() {
+        return firmanavn;
+    }
+
+    public void setFirmanavn(String firmanavn) {
+        this.firmanavn = firmanavn;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public int getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(int tlf) {
+        this.tlf = tlf;
+    }
+
+    public int getCvr_nr() {
+        return cvr_nr;
+    }
+
+    public void setCvr_nr(int cvr_nr) {
+        this.cvr_nr = cvr_nr;
+    }
+
+    public int getRegistrerings_nr() {
+        return registrerings_nr;
+    }
+
+    public void setRegistrerings_nr(int registrerings_nr) {
+        this.registrerings_nr = registrerings_nr;
+    }
+
+    public int getKonto_nr() {
+        return konto_nr;
+    }
+
+    public void setKonto_nr(int konto_nr) {
+        this.konto_nr = konto_nr;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public Postnumre getPost_nr() {
+        return post_nr;
+    }
+
+    public void setPost_nr(Postnumre post_nr) {
+        this.post_nr = post_nr;
+    }
 }
