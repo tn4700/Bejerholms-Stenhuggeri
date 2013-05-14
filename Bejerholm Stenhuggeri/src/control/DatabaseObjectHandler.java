1dce942e70cd472e443fb4bbdc284e8e14455630
Anette Stidsing <anette>
2013-05-14 20:41:11 +0200

    ObjectHandlerKlasse getkunde(int tlf) + getPostNumre()

--- Bejerholm Stenhuggeri/src/control/DatabaseObjectHandler.java
+++ Bejerholm Stenhuggeri/src/control/DatabaseObjectHandler.java
@@ -15,15 +15,16 @@ import model.Postnummer;
  * @author Anette
  */
 public class DatabaseObjectHandler {
+
     private DBConnection db;
-    
-    public DatabaseObjectHandler(DBConnection db){
+
+    public DatabaseObjectHandler(DBConnection db) {
         this.db = db;
     }
-    
-     public ArrayList getPostnumre() throws SQLException {
+
+    public ArrayList getPostnumre() throws SQLException {
         ArrayList<Postnummer> postnummerArray = new ArrayList<Postnummer>();
-        String sql = "select post_nr, by from postnumre";
+        String sql = "select post_nr, by from postnummer";
 
         ResultSet rs;
         rs = db.getData(sql);
@@ -37,30 +38,36 @@ public class DatabaseObjectHandler {
         return postnummerArray;
     }
 
-    
-    
     public Kunde getKunde(int tlf) throws SQLException {
         Kunde kunde = null;
+        Postnummer postnr = null;
         ResultSet rs;
-//        Mangler at tilknytte postnummer
-        String sql = "select fornavn, efternavn, adresse, tlf, post_nr from Kunde where tlf =" + tlf;
-        
+
+        String sql = "select kunde.post_nr, kunde.fornavn, kunde.efternavn, kunde.adresse, kunde.tlf "
+                + "from kunde inner join postnummer on postnummer.post_nr = kunde.post_nr where kunde.tlf=" + tlf;
+
+        try {
+            rs = db.getData(sql);
 
 
-//        try {
-//            rs = db.getData(sql);
-//            if (rs.next()) {
-//                kunde = new Kunde(
-//                        rs.getString("fornavn"),
-//                        rs.getString("efternavn"),
-//                        rs.getString("adresse"),
-//                        rs.getInt("tlf"),
-//                        postnr);
-//                rs.close();
-//            }
-//        }catch(Exception e){
-//            System.out.println("fejl: "+e);
-//        }
+            if (rs.next()) {
+                postnr = new Postnummer(
+                        rs.getInt("post_nr"),
+                        rs.getString("byNavn"));
+                kunde = new Kunde(
+                        rs.getString("fornavn"),
+                        rs.getString("efternavn"),
+                        rs.getString("adresse"),
+                        rs.getInt("tlf"),
+                        postnr);
+                rs.close();
+            }
+        } catch (Exception e) {
+            System.out.println("fejl: " + e);
+        }
         return kunde;
     }
+    
+    
+    
 }
