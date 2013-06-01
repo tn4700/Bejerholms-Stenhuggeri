66e45fe0b6d8693b84bb480202a7c2f5666486d9
Anette Stidsing <anette>
2013-06-02 00:11:01 +0200

    hej

--- Database/4_test.sql
+++ Database/4_test.sql
@@ -1,37 +1,94 @@
 use bejerholmstenhuggeri;
 
--- Test der kontrollerer at primærnøgler og fremmednøgler er korrekt registrerede.
+-- Test der kontrollerer at primærnøgler samt fremmednøgler korrekt registrerede.
+
+
+update ordre
+set ordre_nr = 00009
+where ordre_nr = 00001;
+
+update ordre
+set kirkegård_id = 7
+where kirkegår_id = 3;
+
+update kunde
+set tlf = 29038940
+where tlf = 28931093;
+
+update kunde
+set post_nr = 5695
+where post_nr = 4700;
+
+update vare
+set vare_nr = 50
+where vare_nr = 20;
+
+update vare
+set grp_nr = 9
+where grp_nr = 3;
 
 update faktura
 set ordre_nr = 7
 where ordre_nr = 1;
 
-update ordre
-set ordre_nr = 9
-where ordre_nr = 0;
+update samarbejdspartner
+set tlf = 28282828
+where tlf = 58350001;
 
-select vare_nr, navn, højde, bredde, indkøbspris, salgspris, typenavn, overflade, 
-                dekoration, grp_nr from vare;
+
+
+-- select
+
+select navn, sum(indkøbspris), min(indkøbspris), max(indkøbspris)
+from vare;
+
+
+select vare_nr, navn, sum(salgspris), max(salgspris), min(salgspris)
+from vare
+group by vare_nr
+having max(salgspris) < 850;
+
+select * from kunde where post_nr = 4700;
+
+Select * from kunde
+where efternavn like '%sen%';
+
+
+-- delete
+
+delete from ordre 
+where ordre_nr = '00007';
+
+delete from vare
+where vare_nr = 8;
+
+--  insert 
+
+insert into postnummer(post_nr, byNavn)
+values
+('8981', 'Spentrup'),
+('9000', 'Ålborg');
+
+
+-- update
+
+ update kunde
+set adresse = 'Christiansvej 43'
+where adresse = 'Eventyrvej 27';
+
+
 -- queries der udtrækker data fra mere end en tabel.
 
-select * from (faktura natural join ordre);
+select ordre.ordre_nr, ordre.leveringdato, kunde.tlf, kunde.fornavn, kunde.efternavn 
+from ordre, kunde
+where ordre.tlf = kunde.tlf
+group by ordre.ordre_nr;
 
-select kunde.post_nr,postnummer.bynavn, kunde.fornavn, kunde.efternavn, kunde.adresse, kunde.tlf from kunde join postnummer on kunde.post_nr = postnummer.post_nr where kunde.tlf = 20332836;
+select * 
+from (varegruppe left outer join vare on varegruppe.grp_nr = vare.grp_nr);
 
-select kunde.fornavn, kunde.efternavn, kunde.adresse, kunde.tlf, postnummer.post_nr, postnummer.bynavn from kunde, postnummer where kunde.post_nr = postnummer.post_nr and kunde.tlf = 20332836;
+select varegruppe.navn, vare.navn, vare.vare_nr
+from (varegruppe join vare on varegruppe.grp_nr=vare.grp_nr)
+where varegruppe.grp_nr = 1;
 
-select  kunde.fornavn, kunde.efternavn, kunde.adresse, kunde.tlf, kunde.post_nr, postnummer.bynavn from kunde join postnummer on postnummer.post_nr = kunde.post_nr where kunde.tlf=28931093;
 
-select  samarbejdspartner.firmanavn, samarbejdspartner.adresse, samarbejdspartner.cvr_nr, 
-samarbejdspartner.registrerings_nr, samarbejdspartner.konto_nr, samarbejdspartner.bank, samarbejdspartner.post_nr, postnummer.bynavn
-                from samarbejdspartner join postnummer on postnummer.post_nr = samarbejdspartner.post_nr where samarbejdspartner.cvr_nr = 12943790;
-
-select ordre.ordre_nr, ordre.ordretype, ordre.ordredato, ordre.ordrestatus, ordre.leveringdato, ordre.bemærkning, ordre.leveringsadresse, ordre.kirkegård,
-ordre.afdeling, ordre.afdødnavn, ordre.række, ordre.nummer, ordre.plads_navne, ordre.gravtype from ordre join kunde on kunde.tlf = ordre.tlf where ordre.ordre_nr = 1;
-
-select inskription.inskription, inskription.tegn_id, inskription.skrifttype, tegntype.navn, tegntype.pris_pr_tegn from inskription join tegntype on tegntype.id = inskription.tegn_id;
-
-select vare.vare_nr, vare.navn, vare.højde, vare.bredde, vare.indkøbspris, vare.salgspris, vare.typenavn, vare.overflade, vare.dekoration, vare.grp_nr from vare join varegruppe on varegruppe.grp_nr = vare.grp_nr;
-
-select vare_linje.linje_nr, vare_linje.vare_nr, vare_linje.tom_linje_id, vare_linje.inskription_id, vare_linje.ordre_nr from vare_linje;
- 
