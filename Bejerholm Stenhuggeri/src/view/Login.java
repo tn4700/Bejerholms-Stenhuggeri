945cecbef7e0166890f548b307511b2fc642abd1
Thomas Nielsen <Tozne4700@gmail.com>
2013-06-04 01:59:46 +0200

    Login

--- Bejerholm Stenhuggeri/src/view/Login.java
+++ Bejerholm Stenhuggeri/src/view/Login.java
@@ -12,6 +12,7 @@ import java.awt.Toolkit;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JRootPane;
 import javax.swing.UIManager;
 import model.User;
 
@@ -35,10 +36,16 @@ public class Login extends javax.swing.JFrame {
             e.printStackTrace();
         }  
         initComponents();
+        // set icon til windows toolbar og vinduet
          java.net.URL url = ClassLoader.getSystemResource("img/icon.png");        
         Toolkit kit = Toolkit.getDefaultToolkit();
         Image img = kit.createImage(url);
         this.setIconImage(img);
+        
+       // sæt den til at tryk på login knappen ved klik på enter
+        // sæt Søge knappen til at væred en knap der trykker på når der trykker ENTER på keyboardet
+        JRootPane rootPane = this.getRootPane();
+        rootPane.setDefaultButton(jButton_Login);
         layout = (CardLayout) (jPanel_MainCard.getLayout());
         
       
@@ -70,14 +77,14 @@ public class Login extends javax.swing.JFrame {
 
         jPanel_MainCard = new javax.swing.JPanel();
         jPanel_Login = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        jPanel_LoginRamme = new javax.swing.JPanel();
+        jButton_Login = new javax.swing.JButton();
+        jTextField_Brugernavn = new javax.swing.JTextField();
+        jLabel_BrugerNavn = new javax.swing.JLabel();
+        jLabel_BrugerKode = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JPasswordField();
-        jLabel10 = new javax.swing.JLabel();
+        jTextField_Kodeord = new javax.swing.JPasswordField();
+        jLabel_LoginOverskrift = new javax.swing.JLabel();
         jPanel_DB = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         jTextField_port = new javax.swing.JTextField();
@@ -108,68 +115,68 @@ public class Login extends javax.swing.JFrame {
         jPanel_Login.setBackground(new java.awt.Color(231, 231, 231));
         jPanel_Login.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jPanel_LoginRamme.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
-        jButton1.setText("Login");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jButton_Login.setText("Login");
+        jButton_Login.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jButton_LoginActionPerformed(evt);
             }
         });
 
-        jTextField1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jTextField_Brugernavn.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
 
-        jLabel1.setText("Brugernavn");
+        jLabel_BrugerNavn.setText("Brugernavn");
 
-        jLabel2.setText("Kodeord");
+        jLabel_BrugerKode.setText("Kodeord");
 
-        jTextField2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jTextField_Kodeord.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+        javax.swing.GroupLayout jPanel_LoginRammeLayout = new javax.swing.GroupLayout(jPanel_LoginRamme);
+        jPanel_LoginRamme.setLayout(jPanel_LoginRammeLayout);
+        jPanel_LoginRammeLayout.setHorizontalGroup(
+            jPanel_LoginRammeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_LoginRammeLayout.createSequentialGroup()
                 .addGap(173, 173, 173)
-                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jTextField_Kodeord, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(176, Short.MAX_VALUE))
-            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel2Layout.createSequentialGroup()
+            .addGroup(jPanel_LoginRammeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel_LoginRammeLayout.createSequentialGroup()
                     .addGap(0, 173, Short.MAX_VALUE)
-                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel_LoginRammeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel1)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 219, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel2)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 219, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jLabel_BrugerNavn)
+                        .addComponent(jTextField_Brugernavn, javax.swing.GroupLayout.PREFERRED_SIZE, 219, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel_BrugerKode)
+                        .addComponent(jButton_Login, javax.swing.GroupLayout.PREFERRED_SIZE, 219, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGap(0, 174, Short.MAX_VALUE)))
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+        jPanel_LoginRammeLayout.setVerticalGroup(
+            jPanel_LoginRammeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel_LoginRammeLayout.createSequentialGroup()
                 .addContainerGap(161, Short.MAX_VALUE)
-                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jTextField_Kodeord, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(121, 121, 121))
-            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel2Layout.createSequentialGroup()
+            .addGroup(jPanel_LoginRammeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel_LoginRammeLayout.createSequentialGroup()
                     .addGap(0, 36, Short.MAX_VALUE)
                     .addComponent(jLabel3)
                     .addGap(18, 18, 18)
-                    .addComponent(jLabel1)
+                    .addComponent(jLabel_BrugerNavn)
                     .addGap(11, 11, 11)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField_Brugernavn, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGap(11, 11, 11)
-                    .addComponent(jLabel2)
+                    .addComponent(jLabel_BrugerKode)
                     .addGap(76, 76, 76)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton_Login, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGap(0, 37, Short.MAX_VALUE)))
         );
 
-        jPanel_Login.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, -1, 330));
+        jPanel_Login.add(jPanel_LoginRamme, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, -1, 330));
 
-        jLabel10.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel10.setText("Log ind");
-        jPanel_Login.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 10, -1, -1));
+        jLabel_LoginOverskrift.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel_LoginOverskrift.setText("Log ind");
+        jPanel_Login.add(jLabel_LoginOverskrift, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 10, -1, -1));
 
         jPanel_MainCard.add(jPanel_Login, "Login");
 
@@ -285,12 +292,12 @@ public class Login extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jButton_LoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_LoginActionPerformed
 
         try {
-            User bruger = dbhandler.getUser(jTextField1.getText());
+            User bruger = dbhandler.getUser(jTextField_Brugernavn.getText());
             if (bruger != null) {
-                if (bruger.getPassword().equals(jTextField2.getText())) {
+                if (bruger.getPassword().equals(jTextField_Kodeord.getText())) {
                     jLabel3.setText("Det er den rigtige bruger");
                    MainFrame frame = new MainFrame(db, bruger);
                   frame.show();
@@ -310,7 +317,7 @@ public class Login extends javax.swing.JFrame {
 
 
 
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_jButton_LoginActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         db.setDbNavn(jTextField_dbNavn.getText());
@@ -369,11 +376,8 @@ public class Login extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JButton jButton_Login;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
@@ -381,15 +385,18 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
+    private javax.swing.JLabel jLabel_BrugerKode;
+    private javax.swing.JLabel jLabel_BrugerNavn;
+    private javax.swing.JLabel jLabel_LoginOverskrift;
     private javax.swing.JLabel jLabel_fejl;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel_DB;
     private javax.swing.JPanel jPanel_Login;
+    private javax.swing.JPanel jPanel_LoginRamme;
     private javax.swing.JPanel jPanel_MainCard;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JPasswordField jTextField2;
+    private javax.swing.JTextField jTextField_Brugernavn;
     private javax.swing.JTextField jTextField_Host;
+    private javax.swing.JPasswordField jTextField_Kodeord;
     private javax.swing.JTextField jTextField_dbNavn;
     private javax.swing.JTextField jTextField_port;
     private javax.swing.JTextField jTextField_pw;
